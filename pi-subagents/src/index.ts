3c88aee6eab641d83897b652a8260c425a7c52cc
Vinh Le <lpvinh2k4@gmail.com>
2026-05-11 06:27:32 +1000

    (WIP) Simplify workflow

--- pi-subagents/src/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-import subagentsExtension from "./subagents/index.ts";
-
-export default function combinedExtension(pi: ExtensionAPI) {
-	subagentsExtension(pi);
-}
